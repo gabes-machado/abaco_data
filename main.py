b95504a44d848f9fc4283217ed18f24b4449334d
Gabriel Machado <machad0.gabriel@outlook.com>
2023-09-28 03:14:49 -0300

    commit changes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import time
+import pandas as pd
 
 def driversetup():
     options = webdriver.ChromeOptions()
-    # run Selenium in headless mode
-    
     # overcome limited resource problems
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("lang=en")
@@ -27,26 +25,69 @@ def driversetup():
 
 def scrape(username, password, driver):
     driver = driver
-    driver.get("https://business.facebook.com/")
-    time.sleep(10)
+    
+    time.sleep(5)
+    
+    driver.get("https://business.facebook.com/") # Going to the url
+    time.sleep(5)
 
     driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/div[6]/div/button').click() # Find and click login with Instagram button
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(username) # Find and enter username
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(password) # Find and enter password
-    time.sleep(10)
+    time.sleep(5)
     driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]').click() # Find and click login button
-    time.sleep(10)
-    driver.get("https://business.facebook.com/")
-    time.sleep(10)
+    time.sleep(5)
+    driver.get("https://business.facebook.com/") # Work around the login bug
+    time.sleep(5)
     driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div[2]/div[1]/div/div[1]/div[2]/div/div/div/div[6]/div/button').click() # Find and click login with Instagram button
-    time.sleep(10)
-    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[1]/nav/ul/div/div[7]/div/div/div/li/div/div/a').click()
-    time.sleep(10)
-    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[4]/div[1]/div[1]/div/div/div/div/div[3]/div/div/div').click() # Find and click login with Instagram button
-    time.sleep(10)
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[2]/div/div/div[1]/div/div/div[2]/div/div[1]/nav/ul/div/div[7]/div/div/div/li/div/div/a').click() # Getting in the insights page
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[4]/div[1]/div[1]/div/div/div/div/div[3]/div/div/div').click() # Closing pup-up window
+    time.sleep(5)
+   
+    overview_data = {
+        'username': username,
+        'reach': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div/div/div[2]/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div[2]/div[1]').text,
+        'followers': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[3]/div/div/div[1]/div[2]/div/div/div/div[2]/div[1]/div[3]/div[3]/div[2]/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div[2]/div').text
+        }
+    
+    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div[1]/div[2]/div[1]/div[1]/div/nav[1]/div[2]/div[2]/div[1]/a/div/div').click()
+    time.sleep(5)
 
-    # return print(profile_data)
+    results_data = {
+        'profile_visits': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[1]/div/div/div/span[1]').text,
+        'new_followers': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div[4]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[1]/div/div/div/span[1]').text
+    }
 
-scrape("abacoconsultoriajr", "Abacomaiorem5anos", driversetup())
+    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[1]/div[2]/div/div/div/div/div[1]/div[2]/div[1]/div[1]/div/nav[2]/div[2]/div[1]/div[1]/a/div/div').click()
+    time.sleep(5)
+    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div[3]/div/div/span[1]').click()
+    time.sleep(5)
+
+    content_data = {
+        'post_engagement': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div/div[6]/div/div[2]/div/div[1]/div/div/div/div[2]/div[1]').text
+    }
+
+    driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div[4]/div/div/span[1]').click()
+    time.sleep(5)
+
+    stories_data = {
+        'story_reach': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div/div[2]/div/div[2]/div/div[1]/div/div/div/div[2]/div[1]').text,
+        'story_engagement': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div/div[6]/div/div[2]/div/div[1]/div/div/div/div[2]/div[1]').text,
+        'published_stories': driver.find_element(By.XPATH, '//*[@id="facebook"]/body/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/div/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div/div[4]/div/div[2]/div/div[1]/div/div/div/div[2]/div[1]').text
+    }
+
+    data = overview_data | results_data | content_data | stories_data
+
+    return data
+
+username = str(input("Enter your username: "))
+password = str(input("Enter your password: "))
+
+instagram_data = scrape(username, password, driversetup())
+
+df = pd.DataFrame(instagram_data, index=[0])
+df.to_csv('instagram_data.csv', index=False)
